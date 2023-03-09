558c121ce04e5a4387145303753e52112b0f2e27
WeltonDev <welton.developer95@gmail.com>
2023-03-09 11:15:11 -0300

    scroll smoth

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+const linksMenu = document.querySelectorAll('.menu a[href^="#"]');
 
+// function distanceFromTop(element) {
+//   const id = element.getAttribute("href");
+//   return document.querySelector(id).offsetTop;
+// }
+
+// function scrollToSection(event) {
+//   event.preventDefault();
+//   const element = event.target;
+//   const id = element.getAttribute('href');
+//   const section = document.querySelector(id);
+//   console.log(section);
+// }
+
+// linksMenu.forEach((link)=>{
+//   link.addEventListener('click', scrollToSection)
+// })
 setInterval(()=> {
   let dom = document.querySelector('.home-texto-autor');
   dom.classList.toggle('animate__animated');
